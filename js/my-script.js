932612eb8d5c1ad0ef4b7f6a525c1edaaf2a64a7
gurdeepsingh2000 <gurdeepsingh20000329@gmail.com>
2021-06-14 14:59:15 +0200

    update

--- js/my-script.js
+++ js/my-script.js
@@ -8,7 +8,7 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 //    1. GENERO 5 NUMERI CON UN CICLO FOR PER 5 VOLTE POI ASSEGNARLI AD UN SOLO ALERT.
 
 function randomNum(min,max){
-    return Math.floor(Math.random() * max - min + 1) + min
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 // 2. PREPARO UN'ARRAY DOVE I NUMERI GENERATI CASUALMENTE VERRANNO PUSHATI AL SUO INTERNO COSI IL PROGRAMMA CONTROLLERA SE IL NUMERO INSERITO DALL'UTENTE E IL NUMERO ALL'INTERNO DI QUESTO ARRAY SONO UGUALI.
@@ -18,8 +18,10 @@ var generated_numbers = []
 // 3.GENERO PER 5 VOLTE I NUMERI DA INSERIRE NELL'ARRAY PER POI MOSTRARLI NELL'ALERT
 var len = generated_numbers.length
 for(var x = 0; len < 5 ; x++){
-    var randomizedNum = randomNum(1,50)
-    if(!randomizedNum.includes(generated_numbers)){
-        push.generated_numbers(randomizedNum)
+    var randomizedNum = randomNum(1,50);
+    if(!generated_numbers.includes(randomizedNum)){
+        generated_numbers.push(randomizedNum)
     }
 }
+
+alert('TEST ' + generated_numbers)
